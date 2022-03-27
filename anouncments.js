8e04cd834964308b1d67a8912f12d1bd2362b849
mohammad-hamad55 <88897030+mohammad-hamad55@users.noreply.github.com>
2022-03-28 02:20:10 +0300

    anonucments

--- anouncments.js
+++ anouncments.js
@@ -3,12 +3,13 @@ const readButtons = document.getElementsByClassName("read-button");
 
 // }
 const readMoreHandler = () => {
-  const tempElContent = document.getElementById("temp").content;
-  const tempEl = document.importNode(tempElContent, true);
-  const backGround = tempEl.querySelector("div");
-  const modal = tempEl.querySelectorAll("div")[1];
-  document.body.insertAdjacentElement("afterbegin", backGround);
-  backGround.appendChild(modal);
+  // const tempElContent = document.getElementById("temp").content;
+  // const tempEl = document.importNode(tempElContent, true);
+  // const backGround = tempEl.querySelector("div");
+  // const modal = tempEl.querySelectorAll("div")[1];
+  // document.body.insertAdjacentElement("afterbegin", backGround);
+  // backGround.appendChild(modal);
+  window.open('./blog.html','_self');
 };
 for (const button of readButtons) {
   button.addEventListener("click", readMoreHandler);
