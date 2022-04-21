f9f1f9498b9070ddde0c91eb782e3ee864ea5869
mohammad-hamad55 <88897030+mohammad-hamad55@users.noreply.github.com>
2022-04-21 06:37:53 +0300

    anouncemt flexpilty and responsve

--- anouncments.js
+++ anouncments.js
@@ -1,3 +1,15 @@
+const humbToggler = document.querySelector("header .humb");
+const closeListToggler = document.querySelector("header .close");
+humbToggler.addEventListener("click", () => {
+  humbToggler.classList.add("hidden");
+  closeListToggler.classList.remove("hidden");
+  document.querySelector("header .menu ul").classList.toggle("active");
+});
+closeListToggler.addEventListener("click", () => {
+  closeListToggler.classList.add("hidden");
+  humbToggler.classList.remove("hidden");
+  document.querySelector("header .menu ul").classList.toggle("active");
+});
 const readButtons = document.getElementsByClassName("read-button");
 // const showModal=()=>{
 
